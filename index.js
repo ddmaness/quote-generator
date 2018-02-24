22aef6713250a74bf6d12820e187469dce08d146
Donald Maness <ddmaness@gmail.com>
2018-02-24 18:16:31 -0500

    Complete basic functionality

--- index.js
+++ index.js
@@ -7,6 +7,30 @@ function keyGen(){
 	return key
 }
 
+function randHalf() {
+	return Math.floor((Math.random() * 100) / 2)
+}
+
+function colorGen() {
+	var rgb = [randHalf(), randHalf(), randHalf()];
+	var rgbArr=[(100-rgb[0]) + '%', (100-rgb[1]) + '%',(100-rgb[2]) +'%'];
+	var ZeroToTwo = (Math.floor(Math.random() * 10) % 3);
+	rgbArr[ZeroToTwo] = '0%';
+	var color = 'rgb(' + rgbArr.join(',') + ')';
+	console.log(color);
+	return color;
+}
+
+function applyColor(){
+	var color = colorGen();
+	var container = document.getElementById('container');
+	var quote = document.getElementById('quote');
+	container.style.backgroundColor = color;
+	quote.style.color = color;
+	logo.style.fill = color;
+	refresh.style.stroke =color;
+}
+
 
 // Credit to YDKJS for promise aware generator runner
 function run(gen) {
@@ -59,33 +83,59 @@ function twitterLink(quoteObj) {
 
 function *main() {
 	try {
+		applyColor();
 		var p = yield request();
-		p = p.replace(/\\'/g, '\'')
+		p = p.replace(/\\'/g, '\'');
 		var quote = JSON.parse(p);
-		twitterLink(quote);
-		displayQuote(quote);
+		setTimeout(function(){
+			twitterLink(quote);
+			displayQuote(quote);
+		}, 500)
 	}
 	catch (err) {
 		console.error(err);
+		run(main)
 	}
-	console.log(quote);	
-}
-
-function newQuote() {
-	var btn = document.getElementById('new-quote');
-	btn.addEventListener('click', run(main))
 }
 
 function displayQuote(quoteObj) {
 	var quote = document.getElementById('quote');
-	var author = document.getElementById('author');
-	quote.textContent = quoteObj.quoteText;
-	author.textContent = quoteObj.quoteAuthor;
+	var text = document.getElementById('quote-text');
+	var author = document.getElementById('quote-author');
+	text.textContent = quoteObj.quoteText;
+	quoteObj.quoteAuthor !== '' ? author.textContent = quoteObj.quoteAuthor : author.textContent = 'Unknown';
+	text.classList.add('is-visible');
+	author.classList.add('is-visible');
 }
 
 
+
 run(main);
+
 var btn = document.getElementById('new-quote');
 btn.addEventListener('click', function(){
+	document.getElementById('quote-text').classList.remove('is-visible');
+	document.getElementById('quote-author').classList.remove('is-visible');
 	run(main);
 });
+
+var logo = document.getElementById('twitter-logo');
+logo.addEventListener('mouseover', function(){
+	document.getElementById('twitter-logo-body').classList.add('is-animated');
+	document.getElementById('twitter-logo-wings').classList.add('is-animated');
+})
+
+logo.addEventListener('mouseout', function(){
+	document.getElementById('twitter-logo-body').classList.remove('is-animated');
+	document.getElementById('twitter-logo-wings').classList.remove('is-animated');
+})
+
+var refresh = document.getElementById('new-quote');
+refresh.addEventListener('click', function(){
+	if(!refresh.classList.contains('is-animated')){
+		refresh.classList.add('is-animated');
+		setTimeout(function(){
+			refresh.classList.remove('is-animated');
+		},1500);
+	}
+})
